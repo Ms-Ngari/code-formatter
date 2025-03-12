995c8c8c51ec21f0fb8a5067acaf6a28bbc95b2b
Ms-Ngari <88998578+Ms-Ngari@users.noreply.github.com>
2025-03-12 10:00:56 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
+ls -l
 sh formatter.sh
 
 git add -A && git commit -m "Format Code"
