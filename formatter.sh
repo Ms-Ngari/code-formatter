dd9a365bfe1f2a76999206740be28328c402012f
Ms-Ngari <88998578+Ms-Ngari@users.noreply.github.com>
2025-03-14 09:23:06 +0300

    Update formatter.sh

--- formatter.sh
+++ formatter.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [ "${github.event_name}" == "push" ]; then
-  FILES_CHANGED=$(git diff --name-only ${github.event.before} ${github.sha})
-elif [ "${github.event_name}" == "pull_request" ]; then
-  FILES_CHANGED=$(git diff --name-only ${github.event.pull_request.base.sha} ${github.event.pull_request.head.sha})
+if [ "$GITHUB_EVENT_NAME" == "push" ]; then
+  FILES_CHANGED=$(git diff --name-only $GITHUB_BEFORE $GITHUB_SHA)
+elif [ "$GITHUB_EVENT_NAME" == "pull_request" ]; then
+  FILES_CHANGED=$(git diff --name-only $GITHUB_PR_BASE_SHA $GITHUB_PR_HEAD_SHA)
 fi
 
 echo "Changed files: $FILES_CHANGED"
+echo $GITHUB_EVENT_NAME
 
 JS_FILES=$(echo "$FILES_CHANGED" | grep -E '\.js$|\.ts$|\.json$|\.html$|\.css$' || true)
 PY_FILES=$(echo "$FILES_CHANGED" | grep -E '\.py$' || true)
